82996a906e1802327cf57fa8f0484a625438ac6f
Summer <summer.d.howard@gmail.com>
2020-03-26 10:29:28 -0500

    adding build commands

---
+++ src/intex.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+var world = '🗺️';
+function hello(word) {
+    if (word === void 0) { word = world; }
+    console.log("yes");
+    return "Hello " + world + "! ";
+}
+exports.hello = hello;
+hello("maybe hello");
